b9f35f842ff03389db6f3621d74fa959cdcaac18
OBliteron-code <sssalazar90@gmail.com>
2020-12-16 15:46:11 -0500

    Modificación seeds

--- seeders/20201127211050-user.js
+++ seeders/20201127211050-user.js
@@ -5,7 +5,7 @@ module.exports = {
         return queryInterface.bulkInsert('Usuarios', [{
             nombre: 'carlos',
             email: 'prueba@gmail.com',
-            password: '$2y$08$FTP/jKGNASwJf0ero7SBe.kQmUsOSjWYupPZ6/lS6en6RcithXFKO',
+            password: '$2y$08$yg3YDZw0pKvC8wb53ehfGeXwoxIF6Lb7MtF3Qex.9OM6ic/cooyp2', //micontraseña
             rol: 'Administrador',
             createdAt: new Date(),
             updatedAt: new Date()
